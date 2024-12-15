ad5a3cf3c685776b9c4a81f2f12974717ff2931f
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-12-15 17:20:40 +0100

    add new query

--- src/main/java/com/projecmanagement/service/repository/ProjectRepository.java
+++ src/main/java/com/projecmanagement/service/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package com.projecmanagement.service.repository;
 import com.projecmanagement.service.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project,Integer> {
+    List<Project> findAllByCreatedBy(Integer userId);
 }
